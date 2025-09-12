c2fe49d6ff58b0d66226e7f8968a5a56a1e4cab0
01-Vipinks <ksvipin2001@gmail.com>
2025-09-12 10:49:02 +0530

    Flight_Assignment_Updated

--- Day2/Flight_Assignment/flight.py
+++ Day2/Flight_Assignment/flight.py
@@ -1,62 +1,75 @@
-import json
-import pickle
+import repo_json_dict as repo
 
-class Flight:
-    def __init__(self, id, number, airline_name, seats, price, source, destination):
-        self.id = id
-        self.number = number
-        self.airline_name = airline_name
-        self.seats = seats
-        self.price = price
-        self.source = source
-        self.destination = destination
+def menu():
+    message = '''
+Options are:
+1 - Create Flight
+2 - List All Flight
+3 - Read Flight By Id
+4 - Update Flight
+5 - Delete Fight
+6 - Exit 
+Your Option:'''
+    choice = int(input(message))
+    if choice == 1:
+        id = int(input('ID:'))
+        number = input('Number:')
+        airline_name = input('Airline_name:')
+        capacity = int(input('Capacity:'))
+        price = float(input('Price:'))
+        source = input('Source:')
+        destination = input('Destination:')
 
-    def to_dict(self):
-        return {"id": self.id,
-            "number": self.number,
-            "airline_name": self.airline_name,
-            "seats": self.seats,
-            "price": self.price,
-            "source": self.source,
-            "destination": self.destination}
+        flight = {'id':id, 'number':number, 'airline_name':airline_name, 
+                    'capacity':capacity, 'price':price, 'source': source, 'destination': destination}
 
-def save_to_json(flights, filename="flights.json"):
-    with open(filename, "w") as f:
-        json.dump([flight.to_dict() for flight in flights], f, indent=4)
-    print(f"Flights saved to {filename}")
+        repo.create_flight(flight)
 
-def load_from_json(filename="flights.json"):
-    with open(filename, "r") as f:
-        data = json.load(f)
-    return data
+        print('Flight Created Successfully.')
+    elif choice == 2:
+        print('List of Employees:')
+        for flight in repo.read_all_flight():
+            print(flight)
+    elif choice == 3:
+        id = int(input('ID:'))
+        flight = repo.read_by_id(id)
+        if flight == None:
+            print('Flight not found.')
+        else:
+            print(flight)
+    elif choice == 4:
+        id = int(input('ID:'))
+        flight = repo.read_by_id(id)
+        if flight == None: 
+            print('Flight Not Found')
+        else:
+            print(flight)
+            price = float(input('New Price:'))
+            new_flight = {'id':flight['id'], 
+                'number':flight['number'], 
+                'airline_name':flight['airline_name'], 
+                'price':price, 
+                'capacity':flight['capacity'],
+                'source':flight['source'],
+                'destination':flight['destination']}
+            repo.update(id, new_flight)
+            print('Flight updated successfully.')
+    elif choice == 5:
+        id = int(input('ID:'))
+        flight = repo.read_by_id(id)
+        if flight == None: 
+            print('Flight Not Found')
+        else:
+            repo.delete_flight(id)
+            print('Flight Deleted Succesfully.')
+    elif choice == 6: 
+        print('Thank you for using Application')
 
-def save_to_binary(flights, filename="flights.dat"):
-    with open(filename, "wb") as f:
-        pickle.dump(flights, f)
-    print(f"Flights saved to {filename} (binary)")
+    return choice 
 
-def load_from_binary(filename="flights.dat"):
-    with open(filename, "rb") as f:
-        flights = pickle.load(f)
-    return flights
-
-flights = []
-n = int(input("Enter number of flights: "))
-
-for i in range(n):
-    print(f"\nEnter details for flight {i+1}:")
-    id = int(input("ID: "))
-    number = input("Flight Number: ")
-    airline_name = input("Airline Name: ")
-    seats = int(input("Seats: "))
-    price = float(input("Price: "))
-    source = input("Source: ")
-    destination = input("Destination: ")
-    flights.append(Flight(id, number, airline_name, seats, price, source, destination))
-
-save_to_json(flights)
-print("From JSON:", load_from_json())
-
-save_to_binary(flights)
-binary_flights = load_from_binary()
-print("From Binary:", [f.to_dict() for f in binary_flights])
+def menus():
+    choice = menu()
+    while choice != 6:
+        choice = menu()
+    
+menus()
--- Day3/emp_app_sql_dict_erich/test_repo.py
+++ Day3/emp_app_sql_dict_erich/test_repo.py
@@ -1,4 +1,4 @@
-import pytest
+import pytest 
 from db import db_setup
 from db import repo_sql_dict as repo
 
@@ -7,15 +7,15 @@ def setup():
     db_setup.Base.metadata.drop_all(db_setup.engine)
     #
     db_setup.Base.metadata.create_all(db_setup.engine)
-    yield
+    yield 
     db_setup.Base.metadata.drop_all(db_setup.engine)
-
+    
 def test_create_employee():
-    emp={'id':110, 'name':'Dravid', 'age':50, 'salary':1200, 'is_active':True}
+    emp = {'id':110,'name':'Dravid','age':50,'salary':1200,'is_active':True}
     repo.create_employee(emp)
     #
-    savedEmp=repo.read_by_id(110)
-    assert (savedEmp!=None)
-    assert (savedEmp['id']==110)
-    assert (savedEmp['name']=='Dravid')
-    assert (savedEmp['salary']==1200)
+    savedEmp = repo.read_by_id(110)
+    assert (savedEmp != None)
+    assert (savedEmp['id'] == 110)
+    assert (savedEmp['name'] == 'Dravid')
+    assert (savedEmp['salary'] == 1200)
