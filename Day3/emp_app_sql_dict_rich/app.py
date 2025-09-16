9b534e6ae159f3011d35c1c401c7deabbf8b5159
01-Vipinks <ksvipin2001@gmail.com>
2025-09-16 18:54:55 +0530

    Learning Python

--- Day3/emp_app_sql_dict_rich/app.py
+++ Day3/emp_app_sql_dict_rich/app.py
@@ -1,5 +1,59 @@
 import repo_sql_dict as repo
 
+def create_view():
+    emp_id = int(input('ID:'))
+    name = input('Name:')
+    age = int(input('Age:'))
+    salary = float(input('Salary:'))
+    is_active = input('Active(y/n):').upper() == 'Y'
+
+    employee = {'id':emp_id, 'name':name, 'age':age, 'salary':salary, 'is_active':is_active}
+    try:
+        repo.create_employee(employee)
+        print("Employee created successfully.")
+    except repo.EmployeeAlreadyExistError as ex:
+        print(f"{ex}")
+    except repo.DatabaseError as ex:
+        print(f"{ex}")
+
+def read_all():
+    print('List of Employees:')
+    for employee in repo.read_all_employee():
+        print(employee)
+
+def read_by_id():
+    emp_id = int(input('ID:'))
+    employee = repo.read_by_id(emp_id)
+    if employee is None:
+        print('Employee not found.')
+    else:
+        print(employee)
+
+def update_view():
+    emp_id = int(input('ID:'))
+    employee = repo.read_by_id(emp_id)
+    if employee is None:
+        print('Employee Not Found')
+    else:
+        print(employee)
+        salary = float(input('New Salary:'))
+        new_employee = {'id':employee['id'],
+            'name':employee['name'],
+            'age':employee['age'],
+            'salary':salary,
+            'is_active':employee['is_active']}
+        repo.update(id, new_employee)
+        print('Employee updated successfully.')
+
+def delete_view():
+    emp_id = int(input('ID:'))
+    employee = repo.read_by_id(emp_id)
+    if employee is None:
+        print('Employee Not Found')
+    else:
+        repo.delete_employee(id)
+        print('Employee Deleted Succesfully.')
+
 def menu():
     message = '''
 Options are:
@@ -12,60 +66,18 @@ Options are:
 Your Option:'''
     choice = int(input(message))
     if choice == 1:
-        id = int(input('ID:'))
-        name = input('Name:')
-        age = int(input('Age:'))
-        salary = float(input('Salary:'))
-        is_active = (input('Active(y/n):').upper() == 'Y')
-
-        employee = {'id':id, 'name':name, 'age':age, 
-                    'salary':salary, 'is_active':is_active}
-        try:
-            repo.create_employee(employee)
-            print("Employee created successfully.")
-        except repo.EmployeeAlreadyExistError as ex:
-            print(f"{ex}")
-        except repo.DatabaseError as ex:
-            print(f"{ex}")
-
+        create_view()
     elif choice == 2:
-        print('List of Employees:')
-        for employee in repo.read_all_employee():
-            print(employee)
+        read_all()
     elif choice == 3:
-        id = int(input('ID:'))
-        employee = repo.read_by_id(id)
-        if employee == None:
-            print('Employee not found.')
-        else:
-            print(employee)
+        read_by_id()
     elif choice == 4:
-        id = int(input('ID:'))
-        employee = repo.read_by_id(id)
-        if employee == None: 
-            print('Employee Not Found')
-        else:
-            print(employee)
-            salary = float(input('New Salary:'))
-            new_employee = {'id':employee['id'], 
-                'name':employee['name'], 
-                'age':employee['age'], 
-                'salary':salary, 
-                'is_active':employee['is_active']}
-            repo.update(id, new_employee)
-            print('Employee updated successfully.')
+        update_view()
     elif choice == 5:
-        id = int(input('ID:'))
-        employee = repo.read_by_id(id)
-        if employee == None: 
-            print('Employee Not Found')
-        else:
-            repo.delete_employee(id)
-            print('Employee Deleted Succesfully.')
-    elif choice == 6: 
+        delete_view()
+    elif choice == 6:
         print('Thank you for using Application')
-
-    return choice 
+    return choice
 
 def menus():
     choice = menu()
